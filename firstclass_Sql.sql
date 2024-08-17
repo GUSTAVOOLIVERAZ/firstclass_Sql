b37078f8b8f1f60f99da1b51823d460fdc8fae63
Gustavo Oliveira <142284665+GUSTAVOOLIVERAZ@users.noreply.github.com>
2024-08-16 21:43:12 -0300

    Add files via upload

---
+++ firstclass_Sql.sql
@@ -0,0 +1,17 @@
+-- comentario de uma linha
+
+/*
+comentario de mais de uma
+linha
+*/
+
+-- criacao de um banco de dados
+create database bd2;
+
+
+-- exclusao de um banco de dados
+drop database bd2;
+
+-- selecionando qual banco de dados vamos utilizar 
+use bd2;
+
